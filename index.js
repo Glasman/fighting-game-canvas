a0a18de0f0ec8848bed1af930e9c668ee3980e8f
markglasman <glasman.mark@gmail.com>
2024-10-03 14:55:47 -0400

    successfully changed canvas size

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const canvas = document.querySelector('canvas')
+
+//function that you use to get access to the canvas tags 2D drawing functions
+//c just stands for context
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
