fe0f78593fc59892838ddf806411d992b62f1a41
markglasman <glasman.mark@gmail.com>
2024-10-03 15:13:22 -0400

    defined sprite class and created player sprite

--- index.js
+++ index.js
@@ -6,3 +6,19 @@ const c = canvas.getContext('2d')
 
 canvas.width = 1024
 canvas.height = 576
+
+//canvas context is what's used to draw shapes on the screen, and once we have the canvas context we can start using the canvas api
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+class Sprite {
+    constructor(position) {
+        this.position = position
+    }
+}
+
+const player = new Sprite({
+    x:0,
+    y:0
+})
+
+console.log(player)
