3dc10acdca03ea17805ce34b1ec2a72bc6989ee3
markglasman <glasman.mark@gmail.com>
2024-10-03 15:34:12 -0400

    added enemy sprite, formatted code

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector("canvas");
 
 //function that you use to get access to the canvas tags 2D drawing functions
 //c just stands for context
-const c = canvas.getContext('2d')
+const c = canvas.getContext("2d");
 
-canvas.width = 1024
-canvas.height = 576
+canvas.width = 1024;
+canvas.height = 576;
 
 //canvas context is what's used to draw shapes on the screen, and once we have the canvas context we can start using the canvas api
-c.fillRect(0, 0, canvas.width, canvas.height)
+c.fillRect(0, 0, canvas.width, canvas.height);
 
 class Sprite {
-    constructor(position) {
-        this.position = position
-    }
+  constructor(position) {
+    this.position = position;
+  }
+  //draw() is an arbitrary naming convention, can be named whatever we want
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, 150);
+  }
 }
 
 const player = new Sprite({
-    x:0,
-    y:0
-})
+  x: 0,
+  y: 0,
+});
 
-console.log(player)
+player.draw();
+
+const enemy = new Sprite({
+  x: 400,
+  y: 100,
+});
+
+enemy.draw();
