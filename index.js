a00646367b7ae83011525be34e720b2a55cc515e
markglasman <glasman.mark@gmail.com>
2024-10-08 10:46:51 -0400

    refactored collision code to make it more reusable

--- index.js
+++ index.js
@@ -124,6 +124,18 @@ const keys = {
   },
 };
 
+function rectangularCollision({ rectangle1, rectangle2 }) {
+  return (
+    rectangle1.attackBox.position.x + rectangle1.attackBox.width >=
+      rectangle2.position.x &&
+    rectangle1.attackBox.position.x <=
+      rectangle2.position.x + rectangle2.width &&
+    rectangle1.attackBox.position.y + rectangle1.attackBox.height >=
+      rectangle2.position.y &&
+    rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height
+  );
+}
+
 //arbitrary naming convention, can be named whatever we want
 function animate() {
   //The window.requestAnimationFrame() method tells the browser you wish to perform an animation.
@@ -155,10 +167,10 @@ function animate() {
 
   //detect for collision
   if (
-    player.attackBox.position.x + player.attackBox.width >= enemy.position.x &&
-    player.attackBox.position.x <= enemy.position.x + enemy.width &&
-    player.attackBox.position.y + player.attackBox.height >= enemy.position.y &&
-    player.attackBox.position.y <= enemy.position.y + enemy.height &&
+    rectangularCollision({
+      rectangle1: player,
+      rectangle2: enemy,
+    }) &&
     player.isAttacking
   ) {
     player.isAttacking = false;
