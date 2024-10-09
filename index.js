b1544aab6ca886a48245bd71610e23afea1ccbee
markglasman <glasman.mark@gmail.com>
2024-10-09 12:44:16 -0400

    hitting enemy now brings their health down

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ class Sprite {
     };
     this.color = color;
     this.isAttacking;
+    this.health = 100;
   }
   //draw() is an arbitrary naming convention, can be named whatever we want
   draw() {
@@ -179,7 +180,8 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false;
-    console.log("hit!");
+    enemy.health -= 20;
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
   }
 
   if (
@@ -190,7 +192,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false;
-    console.log("YEOWCH!");
+    player.health -= 20;
+    document.querySelector("#playerHealth").style.width = player.health + "%";
   }
 }
 
