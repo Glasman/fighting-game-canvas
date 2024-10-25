1dd2e7d600e23f19aaeabe4238590a7c72a76a50
markglasman <glasman.mark@gmail.com>
2024-10-24 21:25:53 -0400

    timer stops and winner displayed on timer runout or health = 0

--- index.js
+++ index.js
@@ -142,7 +142,9 @@ function rectangularCollision({ rectangle1, rectangle2 }) {
   );
 }
 
-function determineWinner({player, enemy}) {
+function determineWinner({ player, enemy, timerId }) {
+  clearTimeout(timerId)
+  document.querySelector("#displayText").style.display = "flex";
   if (player.health === enemy.health) {
     document.querySelector("#displayText").innerHTML = "Tie";
   } else if (player.health > enemy.health) {
@@ -152,16 +154,16 @@ function determineWinner({player, enemy}) {
   }
 }
 
-let timer = 4;
+let timer = 30;
+let timerId;
 function decreaseTimer() {
   if (timer > 0) {
-    setTimeout(decreaseTimer, 1000);
+    timerId = setTimeout(decreaseTimer, 1000);
     timer--;
     document.querySelector("#timer").innerHTML = timer;
   }
   if (timer === 0) {
-    document.querySelector("#displayText").style.display = "flex";
-    determineWinner({player, enemy})
+    determineWinner({ player, enemy, timerId });
   }
 }
 decreaseTimer();
@@ -222,7 +224,7 @@ function animate() {
 
   // end game based on health
   if (enemy.health <= 0 || player.health <= 0) {
-
+    determineWinner({ player, enemy, timerId });
   }
 }
 
